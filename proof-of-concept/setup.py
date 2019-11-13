b3afb20481be94fb6df5b7360aabe6bb31006e16
Armin Rigo <arigo@tunes.org>
2019-11-13 13:38:41 +0100

    (antocuni, rlamy, arigo) Start moving things around

--- proof-of-concept/setup.py
+++ proof-of-concept/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, Extension
 
 setup(
     name="hpy-pof",
-    ext_modules = [Extension('pof', ['pof.c'])],
+    ext_modules = [Extension('pof', ['pof.c'], include_dirs=['../src/include'])],
 )
