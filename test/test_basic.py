300a42e5d020b755f888ac80200d19fca0dde198
Antonio Cuni <anto.cuni@gmail.com>
2019-11-13 17:51:29 +0100

    move the @EXPORT closer to the @INIT

--- test/test_basic.py
+++ test/test_basic.py
@@ -13,24 +13,24 @@ class TestBasic(HPyTest):
 
     def test_noop_function(self):
         mod = self.make_module("""
-            @EXPORT test_noop METH_NOARGS
             HPy_FUNCTION(test_noop)
             HPy test_noop_impl(HPyContext ctx, HPy self, HPy args)
             {
                 HPy_RETURN_NONE;
             }
+            @EXPORT test_noop METH_NOARGS
             @INIT
         """)
         assert mod.test_noop() is None
 
     def test_identity_function(self):
         mod = self.make_module("""
-            @EXPORT test_f METH_O
             HPy_FUNCTION(test_f)
             HPy test_f_impl(HPyContext ctx, HPy self, HPy arg)
             {
                 return HPy_Dup(ctx, arg);
             }
+            @EXPORT test_f METH_O
             @INIT
         """)
         x = object()
@@ -38,7 +38,6 @@ class TestBasic(HPyTest):
 
     def test_int_add(self):
         mod = self.make_module("""
-            @EXPORT test_f METH_VARARGS
             HPy_FUNCTION(test_f)
             HPy test_f_impl(HPyContext ctx, HPy self, HPy args)
             {
@@ -47,13 +46,13 @@ class TestBasic(HPyTest):
                     return HPy_NULL;
                 return HPyLong_FromLong(ctx, a + b);
             }
+            @EXPORT test_f METH_VARARGS
             @INIT
         """)
         assert mod.test_f(30, 12) == 42
 
     def test_close(self):
         mod = self.make_module("""
-            @EXPORT test_f METH_O
             HPy_FUNCTION(test_f)
             HPy test_f_impl(HPyContext ctx, HPy self, HPy arg)
             {
@@ -64,18 +63,19 @@ class TestBasic(HPyTest):
                 HPy_Close(ctx, one);
                 return res;
             }
+            @EXPORT test_f METH_O
             @INIT
         """)
         assert mod.test_f(41.5) == 42.5
 
     def test_string(self):
         mod = self.make_module("""
-            @EXPORT test_f METH_NOARGS
             HPy_FUNCTION(test_f)
             HPy test_f_impl(HPyContext ctx, HPy self, HPy args)
             {
                 return HPyUnicode_FromString(ctx, "foobar");
             }
+            @EXPORT test_f METH_NOARGS
             @INIT
         """)
         assert mod.test_f() == "foobar"
