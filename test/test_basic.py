d4ae7e607a5faca899b9a76c34aed6f373794396
Armin Rigo <arigo@tunes.org>
2019-11-14 20:18:50 +0100

    Rename mod.test_f => mod.f

--- test/test_basic.py
+++ test/test_basic.py
@@ -13,48 +13,48 @@ class TestBasic(HPyTest):
 
     def test_noop_function(self):
         mod = self.make_module("""
-            HPy_FUNCTION(test_noop)
-            static HPy test_noop_impl(HPyContext ctx, HPy self, HPy args)
+            HPy_FUNCTION(f)
+            static HPy f_impl(HPyContext ctx, HPy self, HPy args)
             {
                 return HPyNone_Get(ctx);
             }
-            @EXPORT test_noop METH_NOARGS
+            @EXPORT f METH_NOARGS
             @INIT
         """)
-        assert mod.test_noop() is None
+        assert mod.f() is None
 
     def test_identity_function(self):
         mod = self.make_module("""
-            HPy_FUNCTION(test_f)
-            static HPy test_f_impl(HPyContext ctx, HPy self, HPy arg)
+            HPy_FUNCTION(f)
+            static HPy f_impl(HPyContext ctx, HPy self, HPy arg)
             {
                 return HPy_Dup(ctx, arg);
             }
-            @EXPORT test_f METH_O
+            @EXPORT f METH_O
             @INIT
         """)
         x = object()
-        assert mod.test_f(x) is x
+        assert mod.f(x) is x
 
     def test_int_add(self):
         mod = self.make_module("""
-            HPy_FUNCTION(test_f)
-            static HPy test_f_impl(HPyContext ctx, HPy self, HPy args)
+            HPy_FUNCTION(f)
+            static HPy f_impl(HPyContext ctx, HPy self, HPy args)
             {
                 long a, b;
                 if (!HPyArg_ParseTuple(ctx, args, "ll", &a, &b))
                     return HPy_NULL;
                 return HPyLong_FromLong(ctx, a + b);
             }
-            @EXPORT test_f METH_VARARGS
+            @EXPORT f METH_VARARGS
             @INIT
         """)
-        assert mod.test_f(30, 12) == 42
+        assert mod.f(30, 12) == 42
 
     def test_close(self):
         mod = self.make_module("""
-            HPy_FUNCTION(test_f)
-            static HPy test_f_impl(HPyContext ctx, HPy self, HPy arg)
+            HPy_FUNCTION(f)
+            static HPy f_impl(HPyContext ctx, HPy self, HPy arg)
             {
                 HPy one = HPyLong_FromLong(ctx, 1);
                 if (HPy_IsNull(one))
@@ -63,19 +63,19 @@ class TestBasic(HPyTest):
                 HPy_Close(ctx, one);
                 return res;
             }
-            @EXPORT test_f METH_O
+            @EXPORT f METH_O
             @INIT
         """)
-        assert mod.test_f(41.5) == 42.5
+        assert mod.f(41.5) == 42.5
 
     def test_string(self):
         mod = self.make_module("""
-            HPy_FUNCTION(test_f)
-            static HPy test_f_impl(HPyContext ctx, HPy self, HPy args)
+            HPy_FUNCTION(f)
+            static HPy f_impl(HPyContext ctx, HPy self, HPy args)
             {
                 return HPyUnicode_FromString(ctx, "foobar");
             }
-            @EXPORT test_f METH_NOARGS
+            @EXPORT f METH_NOARGS
             @INIT
         """)
-        assert mod.test_f() == "foobar"
+        assert mod.f() == "foobar"
