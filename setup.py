c3ee98c53ee2524ffa28d32baa15e467ddd6010d
Ronan Lamy <ronan.lamy@gmail.com>
2019-11-15 14:24:22 +0100

    Fix packaging of hpy_universal by moving setup.py one level up

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, Extension
+
+setup(
+    name="hpy_universal",
+    ext_modules = [
+        Extension('hpy_universal', ['cpython-universal/src/hpymodule.c', 'cpython-universal/src/api.c'],
+                  include_dirs=['hpy-api/include'])
+    ]
+)
