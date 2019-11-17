4bc3953cdf0a32a49036c9ca370e628037904585
mattip <matti.picus@gmail.com>
2019-11-17 06:01:00 -0700

    define Py_LIMITED_API by default

--- hpy-api/hpy_devel/include/hpy.h
+++ hpy-api/hpy_devel/include/hpy.h
@@ -4,6 +4,13 @@
 #ifdef HPY_UNIVERSAL_ABI
 #    include "universal/hpy.h"
 #else
+/*
+ *   By default, limit the CPython C-API. If you wish to use the
+ *   extended API, you must #include "Python.h" before #including this file
+ */
+#    ifndef Py_PYTHON_H
+#    define Py_LIMITED_API
+#    endif
 #    include "cpython/hpy.h"
 #endif
 
