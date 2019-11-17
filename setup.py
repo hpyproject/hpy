af1b183cea0e87d5add1755368bc0d281ced9cb0
Antonio Cuni <anto.cuni@gmail.com>
2019-11-17 19:18:15 +0100

    Go back to referencing directly the headers.
    It is conceptually wrong to rely on hpy_devel to get the includes, as we are
    IMPLEMENTING the API, not using it.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, Extension
-import hpy_devel
 
 setup(
     name="hpy_universal",
     ext_modules = [
-        Extension('hpy_universal', [
-                      'cpython-universal/src/hpymodule.c',
-                      'cpython-universal/src/handles.c',
-                      'cpython-universal/src/api.c',
-                      ],
-                  include_dirs=[hpy_devel.get_include()])
-    ]
+        Extension('hpy_universal',
+                  ['cpython-universal/src/hpymodule.c',
+                   'cpython-universal/src/handles.c',
+                   'cpython-universal/src/api.c',
+                  ],
+                  include_dirs=['hpy-api/hpy_devel/include'],
+        )]
+
 )
