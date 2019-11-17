169d634d8394e2a9467f74933489b69f0876c8ea
Ronan Lamy <ronan.lamy@gmail.com>
2019-11-17 21:18:12 +0100

    Create script to build a wheel for hpy_pof in a separate virtualenv

---
+++ proof-of-concept/build_wheel_nodeps.sh
@@ -0,0 +1,9 @@
+python -m virtualenv venv/builder
+PY_BUILDER="`pwd`/venv/builder/bin/python3"
+echo "Running with $PY_BUILDER"
+cd ../proof-of-concept
+# ${PY_BUILDER} -m pip install -r requirements.txt
+${PY_BUILDER} -m pip install ../hpy-api
+${PY_BUILDER} setup.py bdist_wheel
+WHEEL=`ls dist/*.whl`
+echo "Created $WHEEL"
