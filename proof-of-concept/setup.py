6eb9286580627c4016da5207f919cfefb5fea1f1
Antonio Cuni <anto.cuni@gmail.com>
2019-11-17 11:05:38 +0100

    update pof to work with the new hpy_devel module

--- proof-of-concept/setup.py
+++ proof-of-concept/setup.py
@@ -1,6 +1,11 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, '../../hpy-api')))
 from setuptools import setup, Extension
+import hpy_devel
 
 setup(
     name="hpy-pof",
-    ext_modules = [Extension('pof', ['pof.c'], include_dirs=['../src/include'])],
+    ext_modules = [
+        Extension('pof', ['pof.c'], include_dirs=[hpy_devel.get_include()]),
+    ]
 )
