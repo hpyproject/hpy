6daeddbd46a247737669ec029fff1f0868f22d07
Antonio Cuni <anto.cuni@gmail.com>
2020-01-31 16:44:56 +0100

    try to fix test_pof on azure

--- proof-of-concept/build_wheel_nodeps.sh
+++ proof-of-concept/build_wheel_nodeps.sh
@@ -4,7 +4,8 @@ PY_BUILDER="`pwd`/venv/builder/bin/python3"
 echo "Running with $PY_BUILDER"
 cd proof-of-concept
 # ${PY_BUILDER} -m pip install -r requirements.txt
-${PY_BUILDER} -m pip install ../hpy-api
+${PY_BUILDER} -m pip install .. # ".." points to the repo root, thus this
+                                # install the whole hpy
 ${PY_BUILDER} setup.py bdist_wheel
 WHEEL=`ls dist/*.whl`
 echo "Created $WHEEL"
