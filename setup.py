9e732ad0da82cb77bc03097aabf53cd5b2e28c1a
Antonio Cuni <anto.cuni@gmail.com>
2020-01-31 16:33:34 +0100

    rename hpy_devel into hpy.devel

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, Extension
 
 setup(
-    name="hpy.universal",
+    name="hpy.devel",
     ext_modules = [
         Extension('hpy.universal',
                   ['hpy/universal/src/hpymodule.c',
                    'hpy/universal/src/handles.c',
                    'hpy/universal/src/api.c',
                   ],
-                  include_dirs=['hpy-api/hpy_devel/include'],
+                  include_dirs=['hpy/devel/include'],
         )]
 
 )
