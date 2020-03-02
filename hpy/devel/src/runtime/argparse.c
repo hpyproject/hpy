4321a8571356a3f971cfdbffaeeb620ac0c98ff0
Simon Cross <hodgestar@gmail.com>
2020-03-02 18:22:28 +0200

    Add const to keywords.

--- hpy/devel/src/runtime/argparse.c
+++ hpy/devel/src/runtime/argparse.c
@@ -45,7 +45,8 @@ HPyArg_Parse(HPyContext ctx, HPy *args, HPy_ssize_t nargs, const char *fmt, ...)
 }
 
 HPyAPI_RUNTIME_FUNC(int)
-HPyArg_ParseKeywords(HPyContext ctx, HPy *args, HPy_ssize_t nargs, HPy kw, const char *fmt, char *keywords[], ...)
+HPyArg_ParseKeywords(HPyContext ctx, HPy *args, HPy_ssize_t nargs, HPy kw,
+                     const char *fmt, const char *keywords[], ...)
 {
   va_list vl;
   va_start(vl, keywords);
