0153d977074b24098c2fd84913e8e470ca51f7e6
Simon Cross <hodgestar@gmail.com>
2020-03-02 18:40:16 +0200

    Add get_sources for HPy runtime.

--- hpy/devel/__init__.py
+++ hpy/devel/__init__.py
@@ -1,5 +1,11 @@
+import glob
 import os
+
 INCLUDE_DIR = os.path.join(os.path.dirname(__file__), 'include')
+SOURCE_DIR = os.path.join(os.path.dirname(__file__), 'src')
 
 def get_include():
     return INCLUDE_DIR
+
+def get_sources():
+    return glob.glob(os.path.join(SOURCE_DIR, "runtime", "*.c"))
