8010ae18556906f7d8e5b97e2e06f6804fd5f474
Antonio Cuni <anto.cuni@gmail.com>
2020-06-10 00:02:06 +0200

    move ctx_module.c to hpy/deve/src/runtime, in preparation to use it in the CPython-ABI mode

---
+++ hpy/devel/include/common/runtime/ctx_module.h
@@ -0,0 +1,9 @@
+#ifndef HPY_COMMON_RUNTIME_CTX_MODULE_H
+#define HPY_COMMON_RUNTIME_CTX_MODULE_H
+
+#include "hpy.h"
+#include "api.h"
+
+_HPy_HIDDEN HPy ctx_Module_Create(HPyContext ctx, HPyModuleDef *hpydef);
+
+#endif /* HPY_COMMON_RUNTIME_CTX_MODULE_H */
