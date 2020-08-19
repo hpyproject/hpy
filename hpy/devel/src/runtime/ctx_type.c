eb43d473fe01cc28bd93e51aa8ca8a563281d5af
Armin Rigo <arigo@tunes.org>
2020-08-19 11:57:56 +0200

    Test passes on cpython

--- hpy/devel/src/runtime/ctx_type.c
+++ hpy/devel/src/runtime/ctx_type.c
@@ -42,11 +42,13 @@ HPyDef_count(HPyDef *defs[], HPyDef_Kind kind)
 
 static void
 legacy_slots_count(PyType_Slot slots[], HPy_ssize_t *slot_count,
-                   PyMethodDef **method_defs, PyMemberDef **member_defs)
+                   PyMethodDef **method_defs, PyMemberDef **member_defs,
+                   PyGetSetDef **getset_defs)
 {
     *slot_count = 0;
     *method_defs = NULL;
     *member_defs = NULL;
+    *getset_defs = NULL;
     if (slots == NULL)
         return;
     for(int i=0; slots[i].slot != 0; i++)
@@ -56,6 +58,9 @@ legacy_slots_count(PyType_Slot slots[], HPy_ssize_t *slot_count,
             break;
         case Py_tp_members:
             *member_defs = (PyMemberDef *)slots[i].pfunc;
+            break;
+        case Py_tp_getset:
+            *getset_defs = (PyGetSetDef *)slots[i].pfunc;
             break;
         default:
             (*slot_count)++;
@@ -172,6 +177,48 @@ create_member_defs(HPyDef *hpydefs[], PyMemberDef *legacy_members)
     return result;
 }
 
+static PyGetSetDef *
+create_getset_defs(HPyDef *hpydefs[], PyGetSetDef *legacy_getsets)
+{
+    HPy_ssize_t hpygetset_count = HPyDef_count(hpydefs, HPyDef_Kind_GetSet);
+    // count the legacy members
+    HPy_ssize_t legacy_count = 0;
+    if (legacy_getsets != NULL) {
+        while (legacy_getsets[legacy_count].name != NULL)
+            legacy_count++;
+    }
+    HPy_ssize_t total_count = hpygetset_count + legacy_count;
+
+    // allocate&fill the result
+    PyGetSetDef *result = PyMem_Calloc(total_count+1, sizeof(PyGetSetDef));
+    if (result == NULL) {
+        PyErr_NoMemory();
+        return NULL;
+    }
+    // copy the HPy members
+    int dst_idx = 0;
+    if (hpydefs != NULL) {
+        for(int i=0; hpydefs[i] != NULL; i++) {
+            HPyDef *src = hpydefs[i];
+            if (src->kind != HPyDef_Kind_GetSet)
+                continue;
+            PyGetSetDef *dst = &result[dst_idx++];
+            /* for Python <= 3.6 compatibility, we need to remove the 'const'
+               qualifier from src->getset.{name,doc} */
+            dst->name = (char *)src->getset.name;
+            dst->get = src->getset.getter_cpy_trampoline;
+            dst->set = src->getset.setter_cpy_trampoline;
+            dst->doc = (char *)src->getset.doc;
+            dst->closure = src->getset.closure;
+        }
+    }
+    // copy the legacy members
+    for(int i=0; i<legacy_count; i++)
+        result[dst_idx++] = legacy_getsets[i];
+    result[dst_idx++] = (PyGetSetDef){NULL};
+    return result;
+}
+
 
 static PyType_Slot *
 create_slot_defs(HPyType_Spec *hpyspec)
@@ -181,11 +228,13 @@ create_slot_defs(HPyType_Spec *hpyspec)
     HPy_ssize_t legacy_slot_count = 0;
     PyMethodDef *legacy_method_defs = NULL;
     PyMemberDef *legacy_member_defs = NULL;
+    PyGetSetDef *legacy_getset_defs = NULL;
     legacy_slots_count(hpyspec->legacy_slots, &legacy_slot_count,
-                       &legacy_method_defs, &legacy_member_defs);
+                       &legacy_method_defs, &legacy_member_defs,
+                       &legacy_getset_defs);
 
-    // add slots to hold Py_tp_methods, Py_tp_members
-    hpyslot_count += 2;
+    // add slots to hold Py_tp_methods, Py_tp_members, Py_tp_getset
+    hpyslot_count += 3;
 
     // allocate the result PyType_Slot array
     HPy_ssize_t total_slot_count = hpyslot_count + legacy_slot_count;
@@ -236,6 +285,16 @@ create_slot_defs(HPyType_Spec *hpyspec)
         return NULL;
     }
     result[dst_idx++] = (PyType_Slot){Py_tp_members, pymembers};
+
+    // add the "real" getsets
+    PyGetSetDef *pygetsets = create_getset_defs(hpyspec->defines, legacy_getset_defs);
+    if (pygetsets == NULL) {
+        PyMem_Free(pymembers);
+        PyMem_Free(pymethods);
+        PyMem_Free(result);
+        return NULL;
+    }
+    result[dst_idx++] = (PyType_Slot){Py_tp_getset, pygetsets};
 
     // add the NULL sentinel at the end
     result[dst_idx++] = (PyType_Slot){0, NULL};
