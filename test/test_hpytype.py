97ba22e42bff38023341319375a1e964680ef344
Antonio Cuni <anto.cuni@gmail.com>
2020-08-24 23:22:20 +0200

    move test_HPyMember up: this way, we can assume that HPyDef_MEMBER works when testing the subsequent features, and simply use p.x and p.y to check for values, instead of the ugly .foo()

--- test/test_hpytype.py
+++ test/test_hpytype.py
@@ -31,14 +31,23 @@ class PointTemplate(DefaultExtensionTemplate):
             static HPy Point_new_impl(HPyContext ctx, HPy cls, HPy *args,
                                       HPy_ssize_t nargs, HPy kw)
             {
+                long x, y;
+                if (!HPyArg_Parse(ctx, args, nargs, "ll", &x, &y))
+                    return HPy_NULL;
                 PointObject *point;
                 HPy h_point = HPy_New(ctx, cls, &point);
                 if (HPy_IsNull(h_point))
                     return HPy_NULL;
-                point->x = 7;
-                point->y = 3;
+                point->x = x;
+                point->y = y;
                 return h_point;
             }
+        """
+
+    def DEFINE_Point_xy(self):
+        return """
+            HPyDef_MEMBER(Point_x, "x", HPyMember_LONG, offsetof(PointObject, x))
+            HPyDef_MEMBER(Point_y, "y", HPyMember_LONG, offsetof(PointObject, y))
         """
 
     def EXPORT_POINT_TYPE(self, *defines):
@@ -54,6 +63,7 @@ class PointTemplate(DefaultExtensionTemplate):
                 .defines = Point_defines
             };
         """ % defines
+
 
 
 class TestType(HPyTest):
@@ -80,7 +90,7 @@ class TestType(HPyTest):
             pass
         assert isinstance(Sub(), mod.Dummy)
 
-    def test_slots(self):
+    def test_HPyDef_SLOT(self):
         mod = self.make_module("""
             HPyDef_SLOT(Dummy_repr, HPy_tp_repr, Dummy_repr_impl, HPyFunc_REPRFUNC);
             static HPy Dummy_repr_impl(HPyContext ctx, HPy self)
@@ -111,7 +121,7 @@ class TestType(HPyTest):
         assert repr(d) == '<Dummy>'
         assert abs(d) == 1234
 
-    def test_tp_methods(self):
+    def test_HPyDef_METH(self):
         import pytest
         mod = self.make_module("""
             HPyDef_METH(Dummy_foo, "foo", Dummy_foo_impl, HPyFunc_O)
@@ -172,31 +182,47 @@ class TestType(HPyTest):
             @EXPORT_POINT_TYPE(&Point_new, &Point_foo)
             @INIT
         """)
-        p = mod.Point()
-        assert p.foo() == 73
+        p1 = mod.Point(7, 3)
+        assert p1.foo() == 73
+        p2 = mod.Point(4, 2)
+        assert p2.foo() == 42
+
+
+    def test_HPyDef_Member(self):
+        mod = self.make_module("""
+            @DEFINE_PointObject
+            @DEFINE_Point_new
+            @DEFINE_Point_xy
+            @EXPORT_POINT_TYPE(&Point_new, &Point_x, &Point_y)
+            @INIT
+        """)
+        p = mod.Point(7, 3)
+        assert p.x == 7
+        assert p.y == 3
+        p.x = 123
+        p.y = 456
+        assert p.x == 123
+        assert p.y == 456
+
 
     def test_HPyType_GenericNew(self):
         mod = self.make_module("""
             @DEFINE_PointObject
+            @DEFINE_Point_xy
 
             HPyDef_SLOT(Point_new, HPy_tp_new, HPyType_GenericNew, HPyFunc_KEYWORDS)
 
-            HPyDef_METH(Point_foo, "foo", Point_foo_impl, HPyFunc_NOARGS)
-            static HPy Point_foo_impl(HPyContext ctx, HPy self)
-            {
-                PointObject *point = HPy_CAST(ctx, PointObject, self);
-                return HPyLong_FromLong(ctx, point->x*10 + point->y);
-            }
-
-            @EXPORT_POINT_TYPE(&Point_new, &Point_foo)
+            @EXPORT_POINT_TYPE(&Point_new, &Point_x, &Point_y)
             @INIT
         """)
         p = mod.Point()
-        assert p.foo() == 0
+        assert p.x == 0
+        assert p.y == 0
 
     def test_tp_init(self):
         mod = self.make_module("""
             @DEFINE_PointObject
+            @DEFINE_Point_xy
 
             HPyDef_SLOT(Point_new, HPy_tp_new, HPyType_GenericNew, HPyFunc_KEYWORDS)
 
@@ -214,18 +240,12 @@ class TestType(HPyTest):
                 return 0;
             }
 
-            HPyDef_METH(Point_foo, "foo", Point_foo_impl, HPyFunc_NOARGS)
-            static HPy Point_foo_impl(HPyContext ctx, HPy self)
-            {
-                PointObject *point = HPy_CAST(ctx, PointObject, self);
-                return HPyLong_FromLong(ctx, point->x*10 + point->y);
-            }
-
-            @EXPORT_POINT_TYPE(&Point_new, &Point_init, &Point_foo)
+            @EXPORT_POINT_TYPE(&Point_new, &Point_init, &Point_x, &Point_y)
             @INIT
         """)
         p = mod.Point(1, 2)
-        assert p.foo() == 12
+        assert p.x == 1
+        assert p.y == 2
 
     def test_sq_item(self):
         mod = self.make_module("""
@@ -310,53 +330,6 @@ class TestType(HPyTest):
         gc.collect()
         assert mod.f() == 7
 
-
-    def test_HPyMember(self):
-        mod = self.make_module("""
-            typedef struct {
-                HPyObject_HEAD
-                long x;
-                long y;
-            } PointObject;
-
-            HPyDef_SLOT(Point_new, HPy_tp_new, Point_new_impl, HPyFunc_KEYWORDS)
-            static HPy Point_new_impl(HPyContext ctx, HPy cls, HPy *args,
-                                      HPy_ssize_t nargs, HPy kw)
-            {
-                PointObject *point;
-                HPy h_point = HPy_New(ctx, cls, &point);
-                if (HPy_IsNull(h_point))
-                    return HPy_NULL;
-                point->x = 7;
-                point->y = 3;
-                return h_point;
-            }
-
-            HPyDef_MEMBER(Point_x, "x", HPyMember_LONG, offsetof(PointObject, x))
-            HPyDef_MEMBER(Point_y, "y", HPyMember_LONG, offsetof(PointObject, y))
-
-            static HPyDef *Point_defines[] = {
-                &Point_new,
-                &Point_x,
-                &Point_y,
-                NULL
-            };
-            static HPyType_Spec Point_spec = {
-                .name = "mytest.Point",
-                .basicsize = sizeof(PointObject),
-                .defines = Point_defines
-            };
-
-            @EXPORT_TYPE("Point", Point_spec)
-            @INIT
-        """)
-        p = mod.Point()
-        assert p.x == 7
-        assert p.y == 3
-        p.x = 123
-        p.y = 456
-        assert p.x == 123
-        assert p.y == 456
 
     def test_HPyDef_GET(self):
         mod = self.make_module("""
