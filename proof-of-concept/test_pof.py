853863831423fa083d9b16ff5afc6548ef8e0a46
Simon Cross <hodgestar@gmail.com>
2020-10-07 16:48:11 +0200

    Fix flake8 issues in proof-of-concept.

--- proof-of-concept/test_pof.py
+++ proof-of-concept/test_pof.py
@@ -1,17 +1,22 @@
 import pof
 
+
 def test_do_nothing():
     assert pof.do_nothing() is None
+
 
 def test_double():
     assert pof.double(21) == 42
 
+
 def test_add_ints():
     assert pof.add_ints(30, 12) == 42
+
 
 def test_add_ints_kw():
     assert pof.add_ints_kw(b=30, a=12) == 42
 
+
 def test_point():
     p = pof.Point(1, 2)
-    assert repr(p) == 'Point(?, ?)' # fixme when we have HPyFloat_FromDouble
+    assert repr(p) == 'Point(?, ?)'  # fixme when we have HPyFloat_FromDouble
