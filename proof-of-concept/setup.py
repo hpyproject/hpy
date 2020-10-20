c1ac8bc0f72457dbcc45f77b8f1fcb940163a8b0
Antonio Cuni <anto.cuni@gmail.com>
2020-10-20 18:58:36 +0200

    start to refactor pof tests, so that it will be easier to test other installation modes from azure-pipelines

--- proof-of-concept/setup.py
+++ proof-of-concept/setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, Extension
 
 setup(
     name="hpy-pof",
+    ## ext_modules=[
+    ##     Extension('foo', sources=['foo.c']),
+    ## ],
     hpy_ext_modules=[
         Extension('pof', sources=['pof.c']),
+#        Extension('bar.foo', sources=['bar/foo.c']),
     ],
     setup_requires=['hpy.devel'],
 )
