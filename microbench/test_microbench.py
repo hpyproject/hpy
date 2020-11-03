fdff457fbb1933bc0535a1b7b3b7637c559d4efb
Antonio Cuni <anto.cuni@gmail.com>
2020-11-03 17:44:12 +0100

    use pytest fixtures to run each microbench against the cpy and hpy versions

--- microbench/test_microbench.py
+++ microbench/test_microbench.py
@@ -3,39 +3,50 @@ These are not real tests, but microbenchmarks. The machinery to record the
 timing and display the results is inside conftest.py
 """
 
+import pytest
 import time
-import cpy_simple
 
-N = 1000000
+@pytest.fixture(params=['cpy', 'hpy'])
+def simple(request):
+    if request.param == 'cpy':
+        import cpy_simple
+        return cpy_simple
+    elif request.param == 'hpy':
+        import hpy_simple
+        return hpy_simple
+    else:
+        assert False, 'Unkown param: %s' % request.param
+
+N = 10000000
 
 class TestModule:
 
-    def test_noargs(self, timer):
+    def test_noargs(self, simple, timer):
         with timer:
             for i in range(N):
-                cpy_simple.noargs()
+                simple.noargs()
 
-    def test_onearg_None(self, timer):
+    def test_onearg_None(self, simple, timer):
         with timer:
             for i in range(N):
-                cpy_simple.onearg(None)
+                simple.onearg(None)
 
-    def test_onearg_int(self, timer):
+    def test_onearg_int(self, simple, timer):
         with timer:
             for i in range(N):
-                cpy_simple.onearg(i)
+                simple.onearg(i)
 
-    def test_varargs(self, timer):
+    def test_varargs(self, simple, timer):
         with timer:
             for i in range(N):
-                cpy_simple.varargs(None, None)
+                simple.varargs(None, None)
 
-    def test_allocate_int(self, timer):
+    def test_allocate_int(self, simple, timer):
         with timer:
             for i in range(N):
-                cpy_simple.allocate_int()
+                simple.allocate_int()
 
-    def test_allocate_tuple(self, timer):
+    def test_allocate_tuple(self, simple, timer):
         with timer:
             for i in range(N):
-                cpy_simple.allocate_tuple()
+                simple.allocate_tuple()
