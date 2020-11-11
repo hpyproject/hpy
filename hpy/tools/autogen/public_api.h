3fd9231b7046358dfb6e30eb81880c100fc101cb
Simon Cross <hodgestar@gmail.com>
2020-11-11 17:15:39 +0200

    Add exception and warning constants.

--- hpy/tools/autogen/public_api.h
+++ hpy/tools/autogen/public_api.h
@@ -18,15 +18,82 @@ typedef int HPyTracker;
 
 /* HPy public API */
 
+/* Constants */
 HPy h_None;
 HPy h_True;
 HPy h_False;
+
+/* Exceptions */
+HPy h_BaseException;
 HPy h_Exception;
+HPy h_StopAsyncIteration;
+HPy h_StopIteration;
+HPy h_GeneratorExit;
+HPy h_ArithmeticError;
+HPy h_LookupError;
+HPy h_AssertionError;
+HPy h_AttributeError;
+HPy h_BufferError;
+HPy h_EOFError;
+HPy h_FloatingPointError;
+HPy h_OSError;
+HPy h_ImportError;
+HPy h_ModuleNotFoundError;
 HPy h_IndexError;
+HPy h_KeyError;
+HPy h_KeyboardInterrupt;
+HPy h_MemoryError;
+HPy h_NameError;
 HPy h_OverflowError;
+HPy h_RuntimeError;
+HPy h_RecursionError;
+HPy h_NotImplementedError;
+HPy h_SyntaxError;
+HPy h_IndentationError;
+HPy h_TabError;
+HPy h_ReferenceError;
 HPy h_SystemError;
+HPy h_SystemExit;
 HPy h_TypeError;
+HPy h_UnboundLocalError;
+HPy h_UnicodeError;
+HPy h_UnicodeEncodeError;
+HPy h_UnicodeDecodeError;
+HPy h_UnicodeTranslateError;
 HPy h_ValueError;
+HPy h_ZeroDivisionError;
+HPy h_BlockingIOError;
+HPy h_BrokenPipeError;
+HPy h_ChildProcessError;
+HPy h_ConnectionError;
+HPy h_ConnectionAbortedError;
+HPy h_ConnectionRefusedError;
+HPy h_ConnectionResetError;
+HPy h_FileExistsError;
+HPy h_FileNotFoundError;
+HPy h_InterruptedError;
+HPy h_IsADirectoryError;
+HPy h_NotADirectoryError;
+HPy h_PermissionError;
+HPy h_ProcessLookupError;
+HPy h_TimeoutError;
+HPy h_EnvironmentError;
+HPy h_IOError;
+
+/* Warnings */
+HPy h_Warning;
+HPy h_UserWarning;
+HPy h_DeprecationWarning;
+HPy h_PendingDeprecationWarning;
+HPy h_SyntaxWarning;
+HPy h_RuntimeWarning;
+HPy h_FutureWarning;
+HPy h_ImportWarning;
+HPy h_UnicodeWarning;
+HPy h_BytesWarning;
+HPy h_ResourceWarning;
+
+/* Types */
 HPy h_BaseObjectType;   /* built-in 'object' */
 HPy h_TypeType;         /* built-in 'type' */
 HPy h_LongType;         /* built-in 'int' */
