dea8890522eb4ad4a53a4934e7bc8de97837f817
Simon Cross <hodgestar@gmail.com>
2020-11-26 18:31:55 +0200

    Update proof-of-concept tests to build and import universal modules as normal.

--- proof-of-concept/test_pof.sh
+++ proof-of-concept/test_pof.sh
@@ -15,18 +15,20 @@ _install_hpy() {
 _test_pof() {
     echo "==== testing pof ===="
     # this assumes that pof is already installed, e.g. after calling
-    # install_pof_wheel
+    # wheel or setup_py_install
     python3 -m pip install pytest pytest-azurepipelines
     cd proof-of-concept
     python3 -m pytest
 }
 
 _build_wheel() {
+    HPY_ABI="$1"
+    VENV="venv/wheel_builder_$HPY_ABI"
     # we use this venv just to build the wheel, and then we install the wheel
     # in the currently active virtualenv
-    echo "Create venv: wheel_builder"
-    python3 -m venv venv/wheel_builder
-    PY_BUILDER="`pwd`/venv/wheel_builder/bin/python3"
+    echo "Create venv: $VENV"
+    python3 -m venv "$VENV"
+    PY_BUILDER="`pwd`/$VENV/bin/python3"
     echo
     echo "Installing hpy and requirements"
     _install_hpy ${PY_BUILDER}
@@ -34,7 +36,7 @@ _build_wheel() {
     ${PY_BUILDER} -m pip install -r requirements.txt
     echo
     echo "Building wheel"
-    ${PY_BUILDER} setup.py bdist_wheel
+    ${PY_BUILDER} setup.py --hpy-abi="$HPY_ABI" bdist_wheel
     popd
 }
 
@@ -50,10 +52,12 @@ _myrm() {
 
 clean() {
     echo "=== cleaning up old stuff ==="
-    _myrm ${ROOT}/venv/wheel_builder
-    _myrm ${ROOT}/venv/wheel_runner
-    _myrm ${ROOT}/venv/cpython_abi
-    _myrm ${ROOT}/venv/universal_abi
+    _myrm ${ROOT}/venv/wheel_builder_cpython
+    _myrm ${ROOT}/venv/wheel_builder_universal
+    _myrm ${ROOT}/venv/wheel_runner_cpython
+    _myrm ${ROOT}/venv/wheel_runner_universal
+    _myrm ${ROOT}/venv/setup_py_install_cpython
+    _myrm ${ROOT}/venv/setup_pu_install_universal
     _myrm ${ROOT}/build
     _myrm ${ROOT}/proof-of-concept/build
     _myrm ${ROOT}/proof-of-concept/dist
@@ -62,56 +66,41 @@ clean() {
 
 wheel() {
     # build a wheel, install and test
+    HPY_ABI="$1"
+    VENV="venv/wheel_runner_$HPY_ABI"
     clean
     echo "=== testing setup.py bdist_wheel ==="
-    _build_wheel
+    _build_wheel "$HPY_ABI"
     WHEEL=`ls proof-of-concept/dist/*.whl`
     echo "Wheel created: ${WHEEL}"
     echo
-    echo "Create venv: wheel_runner"
-    python3 -m venv venv/wheel_runner
-    source venv/wheel_runner/bin/activate
+    echo "Create venv: $VENV"
+    python3 -m venv "$VENV"
+    source "$VENV/bin/activate"
     echo "Installing wheel"
     python3 -m pip install $WHEEL
     echo
     _test_pof
 }
 
-setup_py_cpython() {
-    # install using setup.py, CPython ABI
+setup_py_install() {
+    # install proof-of-concept using setup.py install and run tests
+    HPY_ABI="$1"
+    VENV="venv/setup_py_install_$HPY_ABI"
     clean
-    echo "=== testing setup.py --hpy-abi=cpython install ==="
-    echo "Create venv: cpython_abi"
-    python3 -m venv venv/cpython_abi
-    source venv/cpython_abi/bin/activate
+    echo "=== testing setup.py --hpy-abi=$HPY_ABI install ==="
+    echo "Create venv: $VENV"
+    python3 -m venv "$VENV"
+    source "$VENV/bin/activate"
     _install_hpy python
     echo
     echo "Running setup.py"
     pushd proof-of-concept
-    python3 setup.py --hpy-abi=cpython install
+    python3 setup.py --hpy-abi="$HPY_ABI" install
     popd
     echo
     _test_pof
 }
-
-build_ext_inplace_universal() {
-    # install using setup.py build_ext -i, Universal ABI
-    clean
-    echo "=== testing setup.py --hpy-abi=universal build_ext -i ==="
-    echo "Create venv: universal_abi"
-    python3 -m venv venv/universal_abi
-    source venv/universal_abi/bin/activate
-    _install_hpy python
-    echo
-    echo "Running setup.py"
-    pushd proof-of-concept
-    python3 setup.py --hpy-abi=universal build_ext --inplace
-    popd
-    echo
-    export HPY_UNIVERSAL=1
-    _test_pof
-}
-
 
 # ======== main code =======
 
