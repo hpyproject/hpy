2e28e0886ec93e0602d29b0908b3b82818cf7b3f
Antonio Cuni <anto.cuni@gmail.com>
2020-12-01 10:42:44 +0100

    add a test which fails on pypy

--- test/test_hpyerr.py
+++ test/test_hpyerr.py
@@ -32,3 +32,32 @@ class TestErr(HPyTest):
         """)
         # Calling mod.f() gives a fatal error, ending in abort().
         # How to check that?  For now we just check that the above compiles
+
+    def test_errorval_returned_by_api_functions(self):
+        import pytest
+        mod = self.make_module("""
+            HPyDef_METH(f, "f", f_impl, HPyFunc_VARARGS)
+            static HPy f_impl(HPyContext ctx, HPy self, HPy *args, HPy_ssize_t nargs)
+            {
+                HPy a = HPy_NULL;
+                HPy b = HPy_NULL;
+                HPy res;
+                if (!HPyArg_Parse(ctx, args, nargs, "OO", &a, &b))
+                    return HPy_NULL;
+                res = HPy_TrueDivide(ctx, a, b);
+
+                // the point of the test is to check that in case of error
+                // HPy_Div returns HPy_NULL
+                if (HPy_IsNull(res)) {
+                    HPyErr_Clear(ctx);
+                    return HPyLong_FromLong(ctx, -42);
+                }
+
+                HPy_Close(ctx, b);
+                return res;
+            }
+            @EXPORT(f)
+            @INIT
+        """)
+        assert mod.f(21, 3) == 7
+        assert mod.f(21, 0) == -42
