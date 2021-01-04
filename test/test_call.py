d4954f7e5f301f46600a14ead8b723d5c22ffbbc
Simon Cross <hodgestar@gmail.com>
2021-01-04 18:17:26 +0200

    Update HPy_CallTupleDict tests.

--- test/test_call.py
+++ test/test_call.py
@@ -2,56 +2,26 @@ from .support import HPyTest
 
 
 class TestCall(HPyTest):
-    def test_hpy_call(self):
+    def test_hpy_calltupledict(self):
         import pytest
         mod = self.make_module("""
-            HPyDef_METH(call, "call", call_impl, HPyFunc_VARARGS)
+            HPyDef_METH(call, "call", call_impl, HPyFunc_KEYWORDS)
             static HPy call_impl(HPyContext ctx, HPy self,
-                              HPy *args, HPy_ssize_t nargs)
+                                 HPy *args, HPy_ssize_t nargs, HPy kw)
             {
 
-                HPy f, f_args;
-                HPy f_kw = HPy_NULL;
-                if (!HPyArg_Parse(ctx, NULL, args, nargs, "OO|O", &f, &f_args,
-                                  &f_kw))
-                    return HPy_NULL;
-                return HPy_Call(ctx, f, f_args, f_kw);
-            }
-            @EXPORT(call)
-            @INIT
-        """)
-
-        def f(a, b):
-            return a + b
-
-        assert mod.call(f, (1, 2)) == 3
-        assert mod.call(f, (), {"a": 2, "b": 3}) == 5
-        assert mod.call(str, (2,)) == "2"
-        assert mod.call(str, (2,), {}) == "2"
-        with pytest.raises(TypeError):
-            mod.call(f, (1,))
-        with pytest.raises(TypeError):
-            mod.call(f, (), {"b": 2})
-        with pytest.raises(TypeError):
-            mod.call(f, (), {})
-        with pytest.raises(TypeError):
-            mod.call("not callable", (2,))
-        with pytest.raises(TypeError):
-            mod.call("not callable", (2,), {})
-
-    def test_hpy_callobject(self):
-        import pytest
-        mod = self.make_module("""
-            HPyDef_METH(call, "call", call_impl, HPyFunc_VARARGS)
-            static HPy call_impl(HPyContext ctx, HPy self,
-                              HPy *args, HPy_ssize_t nargs)
-            {
-
-                HPy f;
+                HPy f, result;
                 HPy f_args = HPy_NULL;
-                if (!HPyArg_Parse(ctx, NULL, args, nargs, "O|O", &f, &f_args))
+                HPy f_kw = HPy_NULL;
+                HPyTracker ht;
+                static const char *kwlist[] = { "f", "args", "kw", NULL };
+                if (!HPyArg_ParseKeywords(ctx, &ht, args, nargs, kw, "O|OO",
+                                          kwlist, &f, &f_args, &f_kw)) {
                     return HPy_NULL;
-                return HPy_CallObject(ctx, f, f_args);
+                }
+                result = HPy_CallTupleDict(ctx, f, f_args, f_kw);
+                HPyTracker_Close(ctx, ht);
+                return result;
             }
             @EXPORT(call)
             @INIT
@@ -63,13 +33,26 @@ class TestCall(HPyTest):
         def g():
             return "this is g"
 
-        assert mod.call(f, (1, 2)) == 3
+        # test calls with args (both with and without keywords)
+        assert mod.call(f, args=(1, 2)) == 3
+        assert mod.call(f, args=(), kw={"a": 2, "b": 3}) == 5
+        assert mod.call(g, (), {}) == "this is g"
+        assert mod.call(str, args=(2,)) == "2"
+        assert mod.call(str, args=(2,), kw={}) == "2"
+        with pytest.raises(TypeError):
+            mod.call(f, args=(1,))
+        with pytest.raises(TypeError):
+            mod.call(f, args=(), kw={"b": 2})
+        with pytest.raises(TypeError):
+            mod.call(f, args=(), kw={})
+        with pytest.raises(TypeError):
+            mod.call("not callable", args=(2,))
+        with pytest.raises(TypeError):
+            mod.call("not callable", args=(2,), kw={})
+
+        # test calls without args (both and without keywords)
         assert mod.call(g) == "this is g"
-        assert mod.call(str, (2,)) == "2"
-        with pytest.raises(TypeError):
-            mod.call(f, (1,))
-        with pytest.raises(TypeError):
-            mod.call("not callable", (2,))
+        assert mod.call(g, kw={}) == "this is g"
 
     def test_hpycallable_check(self):
         mod = self.make_module("""
