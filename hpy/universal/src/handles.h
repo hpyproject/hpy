59184b278b1fa322adef78d5928a588c9431736c
Antonio Cuni <anto.cuni@gmail.com>
2021-01-15 13:33:11 +0100

    use +1/-1 instead of ~ for _py2h and _h2py: it's easier to look at them in gdb

--- hpy/universal/src/handles.h
+++ hpy/universal/src/handles.h
@@ -4,19 +4,21 @@
 #include <Python.h>
 #include "hpy.h"
 
-// represent handles as ~address. The main reason is to make sure that if
-// people casts HPy to PyObject* directly, things explode.
+// The main reason for +1/-1 is to make sure that if people casts HPy to
+// PyObject* directly, things explode. Moreover, with this we can easily
+// distinguish normal and debug handles in gdb, by only looking at the last
+// bit.
 
 static inline HPy _py2h(PyObject *obj) {
     if (obj == NULL)
         return HPy_NULL;
-    return (HPy){~(HPy_ssize_t)obj};
+    return (HPy){(HPy_ssize_t)obj + 1};
 }
 
 static inline PyObject *_h2py(HPy h) {
     if HPy_IsNull(h)
         return NULL;
-    return (PyObject *)~h._i;
+    return (PyObject *)(h._i - 1);
 }
 
 #endif /* HPY_HANDLES_H */
