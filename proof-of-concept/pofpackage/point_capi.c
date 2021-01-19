1c29f52948d7d13d311d2b3210cb4802977e41fc
Simon Cross <hodgestar@gmail.com>
2021-01-19 14:25:11 +0200

    Add initial C API Point implementation.

--- proof-of-concept/pofpackage/point_capi.c
+++ proof-of-concept/pofpackage/point_capi.c
@@ -29,30 +29,100 @@ typedef struct {
     double y;
 } PyPointObject;
 
+// this is a method for creating a Point
+int Point_init(PyObject *self, PyObject *args, PyObject *kw)
+{
+    static char *kwlist[] = {"x", "y", NULL};
+    PyPointObject *p = (PyPointObject *)self;
+    p->x = 0.0;
+    p->y = 0.0;
+    if (!PyArg_ParseTupleAndKeywords(args, kw, "|dd", kwlist,
+                                     &p->x, &p->y))
+        return -1;
+    return 0;
+}
+
 // this is a method of Point
 PyObject* Point_norm(PyObject *self)
 {
     PyPointObject *p = (PyPointObject *)self;
     double norm;
-    PyObject result;
-    norm = sqrt(p.x * p.x + p.y * p.y);
+    PyObject *result;
+    norm = sqrt(p->x * p->x + p->y * p->y);
     result = PyFloat_FromDouble(norm);
     return result;
 }
 
 // this is an unrelated function which happens to cast a PyObject* into a
 // PyPointObject*
-PyObject dot(PyObject *point1, PyObject *point2)
+PyObject* dot(PyObject *self, PyObject *args)
 {
+    PyObject *point1, *point2;
+    if (!PyArg_ParseTuple(args, "OO",  &point1, &point2))
+        return NULL;
+
     PyPointObject *p1 = (PyPointObject *)point1;
     PyPointObject *p2 = (PyPointObject *)point2;
-    double db;
-    PyObject result;
-    dp = p1.x * p2.x + p1.y * p2.y;
+
+    double dp;
+    PyObject *result;
+    dp = p1->x * p2->x + p1->y * p2->y;
     result = PyFloat_FromDouble(dp);
     return result;
 }
 
-PyType_Spec PointType_Spec = {
-    .name = "Point",
+
+// Boiler plate for defining the type an
+
+static PyMethodDef PointMethods[] = {
+    {"norm", (PyCFunction)Point_norm, METH_NOARGS, "Distance from origin."},
+    {NULL, NULL, 0, NULL}
+};
+
+static PyType_Slot Point_slots[] = {
+    {Py_tp_doc, "Point (Step 0; C API implementation)"},
+    {Py_tp_init, Point_init},
+    {Py_tp_methods, PointMethods},
+    {0, 0}
+};
+
+static PyType_Spec Point_Type_spec = {
+    .name = "point_capi.Point",
+    .basicsize = sizeof(PyPointObject),
+    .itemsize = 0,
+    .flags = Py_TPFLAGS_DEFAULT,
+    .slots = Point_slots
+};
+
+static PyMethodDef PointModuleMethods[] = {
+    {"dot", (PyCFunction)dot, METH_VARARGS, "Dot product."},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT,
+    "point_capi",
+    "Point module (Step 0; C API implementation)",
+    -1,
+    PointModuleMethods,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+};
+
+PyMODINIT_FUNC
+PyInit_point_capi(void)
+{
+    PyObject* m;
+    m = PyModule_Create(&moduledef);
+    if (m == NULL)
+        return NULL;
+
+    PyObject *point_type = PyType_FromSpec(&Point_Type_spec);
+    if (point_type == NULL)
+        return NULL;
+    PyModule_AddObject(m, "Point", point_type);
+
+    return m;
 }
--- proof-of-concept/setup.py
+++ proof-of-concept/setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, Extension
 
 setup(
     name="hpy-pof",
+    ext_modules=[
+        Extension('pofpackage.point_capi', sources=['pofpackage/point_capi.c'])
+    ],
     hpy_ext_modules=[
         Extension('pof', sources=['pof.c']),
         Extension('pofpackage.foo', sources=['pofpackage/foo.c']),
