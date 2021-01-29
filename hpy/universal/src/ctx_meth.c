36e7b141f013b3f0fc029ceee1375e879795a5ef
Ronan Lamy <ronan.lamy@gmail.com>
2021-01-29 13:30:17 +0000

    Fix trampolines for bf_getbuffer and bf_releasebuffer

--- hpy/universal/src/ctx_meth.c
+++ hpy/universal/src/ctx_meth.c
@@ -2,6 +2,37 @@
 #include "ctx_meth.h"
 #include "handles.h"
 
+static void _buffer_h2py(HPyContext ctx, const HPy_buffer *src, Py_buffer *dest)
+{
+    dest->buf = src->buf;
+    dest->obj = HPy_AsPyObject(ctx, src->obj);
+    dest->len = src->len;
+    dest->itemsize = src->itemsize;
+    dest->readonly = src->readonly;
+    dest->ndim = src->ndim;
+    dest->format = src->format;
+    dest->shape = src->shape;
+    dest->strides = src->strides;
+    dest->suboffsets = src->suboffsets;
+    dest->internal = src->internal;
+}
+
+static void _buffer_py2h(HPyContext ctx, const Py_buffer *src, HPy_buffer *dest)
+{
+    dest->buf = src->buf;
+    dest->obj = HPy_FromPyObject(ctx, src->obj);
+    dest->len = src->len;
+    dest->itemsize = src->itemsize;
+    dest->readonly = src->readonly;
+    dest->ndim = src->ndim;
+    dest->format = src->format;
+    dest->shape = src->shape;
+    dest->strides = src->strides;
+    dest->suboffsets = src->suboffsets;
+    dest->internal = src->internal;
+}
+
+
 HPyAPI_STORAGE void
 ctx_CallRealFunctionFromTrampoline(HPyContext ctx, HPyFunc_Signature sig,
                                    void *func, void *args)
@@ -51,6 +82,25 @@ ctx_CallRealFunctionFromTrampoline(HPyContext ctx, HPyFunc_Signature sig,
        }
        a->result = f(ctx, _py2h(a->self), h_args, nargs, _py2h(a->kw));
        return;
+    }
+    case HPyFunc_GETBUFFERPROC: {
+        HPyFunc_getbufferproc f = (HPyFunc_getbufferproc)func;
+        _HPyFunc_args_GETBUFFERPROC *a = (_HPyFunc_args_GETBUFFERPROC*)args;
+        HPy_buffer hbuf;
+        a->result = f(ctx, _py2h(a->arg0), &hbuf, a->arg2);
+        _buffer_h2py(ctx, &hbuf, a->arg1);
+        HPy_Close(ctx, hbuf.obj);
+        return;
+    }
+    case HPyFunc_RELEASEBUFFERPROC: {
+        HPyFunc_releasebufferproc f = (HPyFunc_releasebufferproc)func;
+        _HPyFunc_args_RELEASEBUFFERPROC *a = (_HPyFunc_args_RELEASEBUFFERPROC*)args;
+        HPy_buffer hbuf;
+        _buffer_py2h(ctx, a->arg1, &hbuf);
+        f(ctx, _py2h(a->arg0), &hbuf);
+        // XXX: copy back from hbuf?
+        HPy_Close(ctx, hbuf.obj);
+        return;
     }
 #include "autogen_ctx_call.i"
     default:
