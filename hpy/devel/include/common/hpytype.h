1551b6106c6b9b72fe80b81d1d92e05c4fd2dcad
Simon Cross <hodgestar@gmail.com>
2021-02-10 15:55:46 +0200

    Update comment on the meaning of HPy_TPFLAGS_LEGACY.

--- hpy/devel/include/common/hpytype.h
+++ hpy/devel/include/common/hpytype.h
@@ -32,9 +32,19 @@ typedef struct {
 
 /* HPy_TPFLAGS_LEGACY is set automatically on legacy types.
 
-   A type is a legacy type if legacy_headersize is greater than zero. This
-   is typically true if PyObject_HEAD is included in the type struct
-   or if the type sets legacy_* slots or methods.
+   A custom type is a legacy type if its struct begins with PyObject_HEAD and
+   it must set .legacy = 1 in its HPyType_Spec.
+
+   A type with .legacy_slots not NULL is required to have .legacy = 1 and to
+   include PyObject_HEAD at the start of its struct. It would be easy to
+   relax this requirement on CPython (where the PyObject_HEAD fields are
+   always present) but a large burden on other implementations (e.g. PyPy,
+   GraalPython) where a struct starting with PyObject_HEAD might not exist.
+
+   Types that do not define a struct of their own, should set the value of
+   .legacy to the same value as the type they inheret from. If they inherit
+   from a built-in type, they may .legacy to either 0 or 1, depending on
+   whether they still use .legacy_slots or not.
 
    Note on the choice of bit 8: Bit 8 looks likely to be the last free TPFLAG
    bit that C Python will allocate. Bits 0 to 8 were dropped in Python 3.0 and
