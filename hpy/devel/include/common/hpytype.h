c58aa41c088a0f6f372e6fcc35fec9057e135686
Simon Cross <hodgestar@gmail.com>
2021-02-15 23:28:18 +0200

    Move HPy_TYPE_HELPERS macros to hpytype.h.

--- hpy/devel/include/common/hpytype.h
+++ hpy/devel/include/common/hpytype.h
@@ -62,4 +62,53 @@ typedef struct {
 /* Set if the type allows subclassing */
 #define HPy_TPFLAGS_BASETYPE (1UL << 10)
 
+
+/* A macro for creating (static inline) helper functions for custom types.
+
+   Two versions of the helper exist. One for legacy types and one for pure
+   HPy types.
+
+   Example for a pure HPy custom type:
+
+       HPyType_HELPERS(PointObject)
+
+   This would generate the following:
+
+   * `PointObject * PointObject_AsStruct(HPyContext ctx, HPy h)`: a static
+     inline function that uses HPy_AsStruct to return the PointObject struct
+     associated with a given handle. The behaviour is undefined if `h`
+     is associated with an object that is not an instance of PointObject.
+
+   * `PointObject_IS_LEGACY`: an enum value set to 0 so that in the
+     HPyType_Spec for PointObject one can write
+     `.legacy = PointObject_IS_LEGACY` and not have to remember to
+     update the spec when the helpers used changes.
+
+   Example for a legacy custom type:
+
+       HPyType_LEGACY_HELPERS(PointObject)
+
+   This would generate the same functions and constants as above, except:
+
+   * `HPy_AsStructLegacy` is used instead of `HPy_AsStruct`.
+
+   * `PointObject_IS_LEGACY` is set to 1.
+*/
+
+#define HPyType_HELPERS(TYPE) \
+    _HPyType_GENERIC_HELPERS(TYPE, HPy_AsStruct, 0)
+
+#define HPyType_LEGACY_HELPERS(TYPE) \
+    _HPyType_GENERIC_HELPERS(TYPE, HPy_AsStructLegacy, 1)
+
+#define _HPyType_GENERIC_HELPERS(TYPE, CAST, IS_LEGACY)              \
+                                                                     \
+enum { TYPE##_IS_LEGACY = IS_LEGACY };                               \
+                                                                     \
+static inline __attribute__((unused)) TYPE *                         \
+TYPE##_AsStruct(HPyContext ctx, HPy h)                               \
+{                                                                    \
+    return (TYPE*) CAST(ctx, h);                                     \
+}
+
 #endif /* HPY_UNIVERSAL_HPYTYPE_H */
