ff62dcdca5fb502c1cc2ad9b79ab90bcda565853
Simon Cross <hodgestar@gmail.com>
2021-03-03 17:23:09 +0200

    Test the name of module pofpackage.foo, not of the package.

--- proof-of-concept/test_pof.py
+++ proof-of-concept/test_pof.py
@@ -19,5 +19,5 @@ def test_point():
     assert repr(p) == 'Point(?, ?)' # fixme when we have HPyFloat_FromDouble
 
 def test_pofpackage():
-    assert pofpackage.__name__ == "pofpackage.foo"
+    assert pofpackage.foo.__name__ == "pofpackage.foo"
     assert pofpackage.foo.hello() == 'hello from pofpackage.foo'
