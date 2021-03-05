678d11b3729344a75ede047b63b95f94b96728a9
Simon Cross <hodgestar@gmail.com>
2021-03-05 14:05:20 +0200

    Update documentation to note that the object the type is added to doesn't have to be a module.

--- hpy/devel/src/runtime/helpers.c
+++ hpy/devel/src/runtime/helpers.c
@@ -2,7 +2,7 @@
  * Runtime helper functions.
  *
  * These are not part of the HPy context or ABI, but are available for
- * HPy extensions to incorporate at compile time.  
+ * HPy extensions to incorporate at compile time.
  *
  * API
  * ---
@@ -12,15 +12,16 @@
 #include "hpy.h"
 
 /**
- * Create a type and add it to the given module. The type is created using
- * `HPyType_FromSpec`.
+ * Create a type and add it as an attribute ot the given object. The type is
+ * created using `HPyType_FromSpec`. The object is often a module that the type
+ * is being added to.
  *
  * :param ctx:
  *     The execution context.
- * :param module:
- *     A handle to the module.
+ * :param obj:
+ *     A handle to the object the type is being added to (often a module).
  * :param name:
- *     The name of the attribute on the module to assign the type to.
+ *     The name of the attribute on the object to assign the type to.
  * :param hpyspec:
  *     The type spec to use to create the type.
  * :param params:
@@ -52,14 +53,14 @@
  *     ...
  */
 HPyAPI_RUNTIME_FUNC(int)
-HPyHelpers_AddType(HPyContext ctx, HPy module, const char *name,
+HPyHelpers_AddType(HPyContext ctx, HPy obj, const char *name,
                   HPyType_Spec *hpyspec, HPyType_SpecParam *params)
 {
     HPy h_type = HPyType_FromSpec(ctx, hpyspec, params);
     if (HPy_IsNull(h_type)) {
         return 0;
     }
-    if (HPy_SetAttr_s(ctx, module, name, h_type) != 0) {
+    if (HPy_SetAttr_s(ctx, obj, name, h_type) != 0) {
         HPy_Close(ctx, h_type);
         return 0;
     }
