605304574446f4b4c6cefd38279da14d33ca53b9
Antonio Cuni <anto.cuni@gmail.com>
2021-07-08 16:55:40 +0100

    try to exit with a clear message in case you try to pip install hpy on a non-cpython

--- setup.py
+++ setup.py
@@ -1,6 +1,35 @@
+import textwrap
 import sys
 import os
 from setuptools import setup, Extension
+
+# this package is supposed to be installed ONLY on CPython. Try to bail out
+# with a meaningful error message in other cases.
+if sys.implementation.name != 'cpython':
+    msg = 'ERROR: Cannot install and/or update hpy on this python implementation:\n'
+    msg += f'    sys.implementation.name == {sys.implementation.name!r}\n\n'
+    if '_hpy_universal' in sys.builtin_module_names:
+        # this is a python which comes with its own hpy implementation
+        import _hpy_universal
+        if hasattr(_hpy_universal, 'get_version'):
+            hpy_version, git_rev = _hpy_universal.get_version()
+            msg += f'This python implementation comes with its own version of hpy=={hpy_version}\n'
+            msg += '\n'
+            msg += 'If you are trying to install hpy through pip, consider to put the\n'
+            msg += 'following in your requirements.txt, to make sure that pip will NOT\n'
+            msg += 'try to re-install it:\n'
+            msg += f'    hpy=={hpy_version}'
+        else:
+            msg += 'This python implementation comes with its own version of hpy,\n'
+            msg += 'but the exact version could not be determined.\n'
+        #
+    else:
+        # this seems to be a python which does not support hpy
+        msg += 'This python implementation does not seem to support hpy:\n'
+        msg += '(built-in module _hpy_universal not found).\n'
+        msg += 'Please contact your vendor for more informations.'
+    sys.exit(msg)
+
 
 if 'HPY_DEBUG' in os.environ:
     # -fkeep-inline-functions is needed to make sure that the stubs for HPy_*
@@ -43,40 +72,38 @@ def get_scm_config():
 
     return {} # use the default config
 
-EXT_MODULES = []
-if sys.implementation.name == 'cpython':
-    EXT_MODULES += [
-        Extension('hpy.universal',
-                  ['hpy/universal/src/hpymodule.c',
-                   'hpy/universal/src/ctx.c',
-                   'hpy/universal/src/ctx_meth.c',
-                   'hpy/universal/src/ctx_misc.c',
-                   'hpy/devel/src/runtime/argparse.c',
-                   'hpy/devel/src/runtime/ctx_call.c',
-                   'hpy/devel/src/runtime/ctx_module.c',
-                   'hpy/devel/src/runtime/ctx_object.c',
-                   'hpy/devel/src/runtime/ctx_type.c',
-                   'hpy/devel/src/runtime/ctx_tracker.c',
-                   'hpy/devel/src/runtime/ctx_listbuilder.c',
-                   'hpy/devel/src/runtime/ctx_tuple.c',
-                   'hpy/devel/src/runtime/ctx_tuplebuilder.c',
-                   'hpy/debug/src/debug_ctx.c',
-                   'hpy/debug/src/debug_ctx_cpython.c',
-                   'hpy/debug/src/debug_handles.c',
-                   'hpy/debug/src/_debugmod.c',
-                   'hpy/debug/src/autogen_debug_wrappers.c',
-                  ],
-                  include_dirs=[
-                      'hpy/devel/include',
-                      'hpy/universal/src',
-                      'hpy/debug/src/include',
-                  ],
-                  extra_compile_args=[
-                      '-DHPY_UNIVERSAL_ABI',
-                      '-DHPY_DEBUG_ENABLE_UHPY_SANITY_CHECK',
-                  ] + EXTRA_COMPILE_ARGS
-                  )
-        ]
+EXT_MODULES = [
+    Extension('hpy.universal',
+              ['hpy/universal/src/hpymodule.c',
+               'hpy/universal/src/ctx.c',
+               'hpy/universal/src/ctx_meth.c',
+               'hpy/universal/src/ctx_misc.c',
+               'hpy/devel/src/runtime/argparse.c',
+               'hpy/devel/src/runtime/ctx_call.c',
+               'hpy/devel/src/runtime/ctx_module.c',
+               'hpy/devel/src/runtime/ctx_object.c',
+               'hpy/devel/src/runtime/ctx_type.c',
+               'hpy/devel/src/runtime/ctx_tracker.c',
+               'hpy/devel/src/runtime/ctx_listbuilder.c',
+               'hpy/devel/src/runtime/ctx_tuple.c',
+               'hpy/devel/src/runtime/ctx_tuplebuilder.c',
+               'hpy/debug/src/debug_ctx.c',
+               'hpy/debug/src/debug_ctx_cpython.c',
+               'hpy/debug/src/debug_handles.c',
+               'hpy/debug/src/_debugmod.c',
+               'hpy/debug/src/autogen_debug_wrappers.c',
+              ],
+              include_dirs=[
+                  'hpy/devel/include',
+                  'hpy/universal/src',
+                  'hpy/debug/src/include',
+              ],
+              extra_compile_args=[
+                  '-DHPY_UNIVERSAL_ABI',
+                  '-DHPY_DEBUG_ENABLE_UHPY_SANITY_CHECK',
+              ] + EXTRA_COMPILE_ARGS
+              )
+    ]
 
 
 DEV_REQUIREMENTS = [
