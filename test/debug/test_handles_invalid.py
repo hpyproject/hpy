f79eef4c01b9f942ef264dd48748c23a4d27e291
stepan <stepan.sindelar@oracle.com>
2021-10-21 16:00:25 +0200

    Add few more tests to test_handles_invalid

--- test/debug/test_handles_invalid.py
+++ test/debug/test_handles_invalid.py
@@ -8,6 +8,30 @@ def hpy_abi():
         yield "debug"
 
 
+def test_no_invalid_handle(compiler, hpy_debug_capture):
+    # Basic sanity check that valid code does not trigger any error reports
+    mod = compiler.make_module("""
+        HPyDef_METH(f, "f", f_impl, HPyFunc_O)
+        static HPy f_impl(HPyContext *ctx, HPy self, HPy arg)
+        {
+            HPy x = HPyLong_FromLong(ctx, 42);
+            HPy y = HPyLong_FromLong(ctx, 2);
+            HPy arg_dup = HPy_Dup(ctx, arg);
+            HPy_Close(ctx, y);
+            HPy b = HPy_Dup(ctx, x);
+            HPy_Close(ctx, x);
+            HPy_Close(ctx, arg_dup);
+            return b;
+        }
+
+        @EXPORT(f)
+        @INIT
+    """)
+    assert mod.f("hello") == 42
+    assert mod.f("world") == 42
+    assert hpy_debug_capture.invalid_handles_count == 0
+
+
 def test_cant_use_closed_handle(compiler, hpy_debug_capture):
     mod = compiler.make_module("""
         HPyDef_METH(f, "f", f_impl, HPyFunc_O, .doc="double close")
@@ -35,6 +59,33 @@ def test_cant_use_closed_handle(compiler, hpy_debug_capture):
     assert hpy_debug_capture.invalid_handles_count == 1
     mod.g('bar')   # use-after-close
     assert hpy_debug_capture.invalid_handles_count == 2
+
+
+def test_keeping_and_reusing_argument_handle(compiler, hpy_debug_capture):
+    mod = compiler.make_module("""
+        HPy keep;
+
+        HPyDef_METH(f, "f", f_impl, HPyFunc_O)
+        static HPy f_impl(HPyContext *ctx, HPy self, HPy arg)
+        {
+            keep = arg;
+            return HPy_Dup(ctx, ctx->h_None);
+        }
+
+        HPyDef_METH(g, "g", g_impl, HPyFunc_NOARGS)
+        static HPy g_impl(HPyContext *ctx, HPy self)
+        {
+            return keep;
+        }
+
+        @EXPORT(f)
+        @EXPORT(g)
+        @INIT
+    """)
+    mod.f("hello leaks!")
+    assert hpy_debug_capture.invalid_handles_count == 0
+    mod.g()
+    assert hpy_debug_capture.invalid_handles_count == 1
 
 
 def test_invalid_handle_crashes_python_if_no_hook(compiler, python_subprocess, fatal_exit_code):
