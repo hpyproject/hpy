f7d6d4f69165306d1058ffb1c7c73a4b74c99ebe
mattip <matti.picus@gmail.com>
2022-05-05 12:44:43 +0300

    move HPyTuple_Pack from a macro to an inline function

--- hpy/devel/include/hpy/inline_helpers.h
+++ hpy/devel/include/hpy/inline_helpers.h
@@ -11,4 +11,27 @@ HPyAPI_FUNC HPy HPyErr_SetFromErrnoWithFilenameObject(HPyContext *ctx, HPy h_typ
     return HPyErr_SetFromErrnoWithFilenameObjects(ctx, h_type, filename, HPy_NULL);
 }
 
+HPyAPI_FUNC HPy HPyTuple_Pack(HPyContext *ctx, HPy_ssize_t n, ...) {
+    va_list vargs;
+    HPy_ssize_t i;
+
+    if (n == 0) {
+        HPy array[] = {};
+        return HPyTuple_FromArray(ctx, array, n);
+    }
+    HPy *array = (HPy*)malloc(n * sizeof(HPy));
+    va_start(vargs, n);
+    if (array == NULL) {
+        va_end(vargs);
+        return HPy_NULL;
+    }
+    for (i = 0; i < n; i++) {
+        array[i] = va_arg(vargs, HPy);
+    }
+    va_end(vargs);
+    HPy result = HPyTuple_FromArray(ctx, array, n);
+    free(array);
+    return result;
+}
+
 #endif //HPY_INLINE_HELPERS_H
