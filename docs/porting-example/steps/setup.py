8d93e75ec8f22739dd50233cb8ff49a7022ffec8
Łukasz Langa <lukasz@langa.pl>
2022-09-19 16:34:38 +0200

    Make setup.py dumber but less magical

--- docs/porting-example/steps/setup.py
+++ docs/porting-example/steps/setup.py
@@ -6,10 +6,18 @@ from conftest import PORTING_STEPS
 
 
 setup(
-    name="porting-example",
+    name="hpy-porting-example",
+    ext_modules=[
+        Extension("step_00_c_api", sources=["step_00_c_api.c"])
+    ],
     hpy_ext_modules=[
-        Extension(step.name, sources=[step.src])
-        for step in PORTING_STEPS
+        Extension(name, sources=[f"{name}.c"])
+        for name in
+        [
+            "step_01_hpy_legacy",
+            "step_02_hpy_legacy",
+            "step_03_hpy_final",
+        ]
     ],
     setup_requires=['hpy'],
 )
