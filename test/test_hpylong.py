28700e32e3c3f168ab98712e8c9b12c3562a3ed8
Florian Angerer <florian.angerer@oracle.com>
2022-12-28 14:51:02 +0100

    Add tests for HPyLong_(From/As)(Int32/UInt43/Int64/UInt64/..)

--- test/test_hpylong.py
+++ test/test_hpylong.py
@@ -1,6 +1,36 @@
-from .support import HPyTest
+from .support import HPyTest, DefaultExtensionTemplate
+
+class HPyLongTemplate(DefaultExtensionTemplate):
+    def DEFINE_Long_From(self, type, api_suffix, val):
+        self.EXPORT("from_{type}_{val}".format(type=type, val=val))
+        return """
+            HPyDef_METH(from_{type}_{val}, "from_{type}_{val}", HPyFunc_NOARGS)
+            static HPy from_{type}_{val}_impl(HPyContext *ctx, HPy self)
+            {{
+                {type} a = {val};
+                return HPyLong_From{api_suffix}(ctx, a);
+            }}
+        """.format(type=type, val=val, api_suffix=api_suffix)
+
+    def DEFINE_Long_As(self, type, api_suffix, from_suffix=None):
+        self.EXPORT("as_{api_suffix}".format(api_suffix=api_suffix))
+        if not from_suffix:
+            from_suffix = api_suffix
+        return """
+            HPyDef_METH(as_{api_suffix}, "as_{api_suffix}", HPyFunc_O)
+            static HPy as_{api_suffix}_impl(HPyContext *ctx, HPy self, HPy arg)
+            {{
+                {type} a = HPyLong_As{api_suffix}(ctx, arg);
+                if (a == (({type})-1) && HPyErr_Occurred(ctx))
+                    return HPy_NULL;
+                return HPyLong_From{from_suffix}(ctx, a * 2);
+            }}
+        """.format(type=type, api_suffix=api_suffix, from_suffix=from_suffix)
 
 class TestLong(HPyTest):
+
+    ExtensionTemplate = HPyLongTemplate
+
     def unsigned_long_bits(self):
         """ Return the number of bits in an unsigned long. """
         import struct
@@ -44,6 +74,23 @@ class TestLong(HPyTest):
         assert vi.major >= 3
         return (vi.major == 3 and vi.minor <= 9)
 
+    def test_Long_FromFixedWidth(self):
+        mod = self.make_module("""
+            @DEFINE_Long_From(int32_t, Int32, INT32_MAX)
+            @DEFINE_Long_From(int32_t, Int32, INT32_MIN)
+            @DEFINE_Long_From(uint32_t, UInt32, UINT32_MAX)
+            @DEFINE_Long_From(int64_t, Int64, INT64_MAX)
+            @DEFINE_Long_From(int64_t, Int64, INT64_MIN)
+            @DEFINE_Long_From(uint64_t, UInt64, UINT64_MAX)
+            @INIT
+        """)
+        assert mod.from_int32_t_INT32_MAX() == 2147483647
+        assert mod.from_int32_t_INT32_MIN() == -2147483648
+        assert mod.from_uint32_t_UINT32_MAX() == 4294967295
+        assert mod.from_int64_t_INT64_MAX() == 9223372036854775807
+        assert mod.from_int64_t_INT64_MIN() == -9223372036854775808
+        assert mod.from_uint64_t_UINT64_MAX() == 18446744073709551615
+
     def test_Long_FromLong(self):
         mod = self.make_module("""
             HPyDef_METH(f, "f", HPyFunc_NOARGS)
@@ -56,6 +103,51 @@ class TestLong(HPyTest):
             @INIT
         """)
         assert mod.f() == 500
+
+    def test_Long_AsFixedWidth(self):
+        import pytest
+        mod = self.make_module("""
+            @DEFINE_Long_As(int32_t, Int32)
+            @DEFINE_Long_As(uint32_t, UInt32)
+            @DEFINE_Long_As(uint32_t, UInt32Mask, UInt32)
+            @DEFINE_Long_As(int64_t, Int64)
+            @DEFINE_Long_As(uint64_t, UInt64)
+            @DEFINE_Long_As(uint64_t, UInt64Mask, UInt64)
+            @INIT
+        """)
+
+        assert mod.as_Int32(45) == 90
+        assert mod.as_Int32(-45) == -90
+        # doubling INT32_MAX is like a left-shift and will result in a negative value
+        assert mod.as_Int32(2147483647) < 0
+        with pytest.raises(TypeError):
+            mod.as_Int32("this is not a number")
+        if self.python_supports_magic_int():
+            assert mod.as_Int32(self.magic_int(2)) == 4
+        if self.python_supports_magic_index():
+            assert mod.as_Int32(self.magic_index(2)) == 4
+
+        assert mod.as_UInt32(45) == 90
+        with pytest.raises(OverflowError):
+            mod.as_UInt32(-45)
+        assert mod.as_UInt32(2147483647) == 4294967294
+
+        assert mod.as_UInt32Mask(0xffffffffffffffff) == 0xfffffffe
+
+        assert mod.as_Int64(45) == 90
+        assert mod.as_Int64(-45) == -90
+        # doubling INT32_MAX is like a left-shift and will result in a negative value
+        assert mod.as_Int64(2147483647) == 4294967294
+        assert mod.as_Int64(9223372036854775807) < 0
+        with pytest.raises(TypeError):
+            mod.as_Int64("this is not a number")
+
+        assert mod.as_UInt64(45) == 90
+        with pytest.raises(OverflowError):
+            mod.as_UInt64(-45)
+        assert mod.as_UInt64(9223372036854775807) == 18446744073709551614
+
+        assert mod.as_UInt64Mask(0xffffffffffffffffff) == 0xfffffffffffffffe
 
     def test_Long_AsLong(self):
         import pytest
