702ff1a697c6ba99f66c2f3b942481e0eadba5e6
stepan <stepan.sindelar@oracle.com>
2023-02-01 16:09:06 +0100

    Autogen code also for HPyFunc_NOARGS and HPyFunc_NOARGS_O

--- hpy/universal/src/ctx_meth.c
+++ hpy/universal/src/ctx_meth.c
@@ -39,18 +39,6 @@ ctx_CallRealFunctionFromTrampoline(HPyContext *ctx, HPyFunc_Signature sig,
                                    void* (*func)(), void *args)
 {
     switch (sig) {
-    case HPyFunc_NOARGS: {
-        HPyFunc_noargs f = (HPyFunc_noargs)func;
-        _HPyFunc_args_NOARGS *a = (_HPyFunc_args_NOARGS*)args;
-        a->result = _h2py(f(ctx, _py2h(a->self)));
-        return;
-    }
-    case HPyFunc_O: {
-        HPyFunc_o f = (HPyFunc_o)func;
-        _HPyFunc_args_O *a = (_HPyFunc_args_O*)args;
-        a->result = _h2py(f(ctx, _py2h(a->self), _py2h(a->arg)));
-        return;
-    }
     case HPyFunc_VARARGS: {
         HPyFunc_varargs f = (HPyFunc_varargs)func;
         _HPyFunc_args_VARARGS *a = (_HPyFunc_args_VARARGS*)args;
