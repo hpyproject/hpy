534ae125fb91b052a75722d003385d78c6c6ed20
Florian Angerer <florian.angerer@oracle.com>
2023-03-01 15:10:38 +0100

    Make Infer (static analysis) happy

--- hpy/devel/src/runtime/ctx_type.c
+++ hpy/devel/src/runtime/ctx_type.c
@@ -1382,6 +1382,7 @@ _HPy_HIDDEN HPyType_BuiltinShape ctx_Type_GetBuiltinShape(HPyContext *ctx, HPy h
 _HPy_HIDDEN const char *ctx_Type_GetName(HPyContext *ctx, HPy type)
 {
     PyTypeObject *tp = (PyTypeObject*) _h2py(type);
+    assert(tp != NULL);
     if (tp->tp_flags & Py_TPFLAGS_HEAPTYPE) {
         PyHeapTypeObject* et = (PyHeapTypeObject*)tp;
         return PyUnicode_AsUTF8(et->ht_name);
