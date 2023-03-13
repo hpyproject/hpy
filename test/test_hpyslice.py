f7e560ae59ec298f079deff6c80adca5020f70f8
Florian Angerer <florian.angerer@oracle.com>
2023-03-13 17:01:58 +0100

    Add test for HPySlice_AdjustIndices

--- test/test_hpyslice.py
+++ test/test_hpyslice.py
@@ -55,3 +55,56 @@ class TestSlice(HPyTest):
             mod.f(slice(1, "10", 1))
         with pytest.raises(TypeError):
             mod.f(slice("0", 10, 1))
+
+    def test_adjust_indices(self):
+        mod = self.make_module("""
+            HPyDef_METH(f, "f", HPyFunc_VARARGS)
+            static HPy f_impl(HPyContext *ctx, HPy self, HPy *args, HPy_ssize_t nargs)
+            {
+                HPy_ssize_t length, start, stop, step;
+
+                if (nargs != 4) {
+                    HPyErr_SetString(ctx, ctx->h_TypeError,
+                        "expected exactly 4 arguments");
+                    return HPy_NULL;
+                }
+
+                if (HPyArg_Parse(ctx, NULL, args, nargs, "nnnn",
+                        &length, &start, &stop, &step) < 0) {
+                    return HPy_NULL;
+                }
+                length = HPySlice_AdjustIndices(ctx, length, &start, &stop, step);
+                return HPy_BuildValue(ctx, "nnnn", length, start, stop, step);
+            }
+            @EXPORT(f)
+            @INIT
+        """)
+
+        # For the following tests, assume we work on a list:
+        # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        assert mod.f(10, 0, 10, 1) == (10, 0, 10, 1)
+        assert mod.f(10, 0, -1, 1) == (9, 0, 9, 1)
+        assert mod.f(10, -10, -1, 1) == (9, 0, 9, 1)
+        assert mod.f(10, 15, 20, 1) == (0, 10, 10, 1)
+        # start at index 9 and to go index 1 (inclusive; since 0 is exclusive)
+        assert mod.f(10, 9, 0, -1) == (9, 9, 0, -1)
+        assert mod.f(10, -1, -10, -1) == (9, 9, 0, -1)
+        assert mod.f(10, 5, 5, -1) == (0, 5, 5, -1)
+
+        # same as above but with step 2
+        assert mod.f(10, 0, 10, 2) == (5, 0, 10, 2)
+        assert mod.f(10, 0, -1, 2) == (5, 0, 9, 2)
+        assert mod.f(10, -10, -1, 2) == (5, 0, 9, 2)
+        assert mod.f(10, 15, 20, 2) == (0, 10, 10, 2)
+        assert mod.f(10, 9, 0, -2) == (5, 9, 0, -2)
+        assert mod.f(10, -1, -10, -2) == (5, 9, 0, -2)
+        assert mod.f(10, 5, 5, -2) == (0, 5, 5, -2)
+
+        # same as above but with step 3
+        assert mod.f(10, 0, 10, 3) == (4, 0, 10, 3)
+        assert mod.f(10, 0, -1, 3) == (3, 0, 9, 3)
+        assert mod.f(10, -10, -1, 3) == (3, 0, 9, 3)
+        assert mod.f(10, 15, 20, 3) == (0, 10, 10, 3)
+        assert mod.f(10, 9, 0, -3) == (3, 9, 0, -3)
+        assert mod.f(10, -1, -10, -3) == (3, 9, 0, -3)
+        assert mod.f(10, 5, 5, -3) == (0, 5, 5, -3)
