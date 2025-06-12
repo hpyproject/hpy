c31603eb77ca109b084230b9e4976e903c463481
paugier <pierre.augier@univ-grenoble-alpes.fr>
2025-06-12 15:49:53 +0200

    microbench/profile_alloc: heap type + cpy for PyPy

--- microbench/profile_alloc/profile_alloc.py
+++ microbench/profile_alloc/profile_alloc.py
@@ -6,6 +6,8 @@ from time import perf_counter as time
 
 if "purepy" in sys.argv:
     import purepy_simple as simple
+elif "cpy" in sys.argv:
+    import cpy_simple as simple
 else:
     if sys.implementation.name == "cpython":
         import cpy_simple as simple
@@ -14,7 +16,7 @@ else:
 
 print(simple)
 
-Foo = simple.Foo
+cls = simple.HTFoo
 N = 10000000
 if sys.implementation.name == "cpython":
     N *= 10
@@ -22,11 +24,11 @@ if sys.implementation.name == "cpython":
 def main():
     objs = [None] * N
     for i in range(N):
-        objs[i] = Foo()
+        objs[i] = cls()
     return objs
 
 gc.collect()
 
 t_start = time()
 main()
-print(f"time per allocation: {(time() - t_start)/N:.2g} s")
+print(f"time per allocation: {(time() - t_start)/N:.1e} s")
